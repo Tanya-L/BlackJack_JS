c460e20a830dcb6e8745b8a720d6f51386f28973
Tanya Ly <tanya.lytovchenko@gmail.com>
2019-03-17 00:34:22 +0100

    game done

--- black-jack.js
+++ black-jack.js
@@ -22,8 +22,6 @@ function shuffleDeck() {
         deck.push(...card);
     }
 };
-shuffleDeck();
-console.log(deck);
 
 // Draw a Card
 function drawCard() {
@@ -42,12 +40,35 @@ let playerCards = "";
 let dealerCards = "";
 let playerScore = 0;
 let dealerScore = 0;
+let newGame = document.getElementById("new-game-button");
+let hit = document.getElementById("hit-button");
+let stay = document.getElementById("stay-button");
+let winnerArea = document.getElementById("winner-area");
 
+// add click event
+newGame.addEventListener("click", startNewGame);
+
+hideGameButton();
+
+hit.addEventListener("click", function () {
+    player.push(drawCard());
+    showHands();
+});
+
+stay.addEventListener("click", function () {
+    hideGameButton();
+    while (dealerScore < playerScore && playerScore <= 21 && dealerScore <= 21) {
+        dealer.push(drawCard());
+        showHands(true);
+    }
+});
+
+
+// cards
 function dealInitialCards() {
     dealer.push(drawCard());
     player.push(drawCard());
-    playerScore = calculateHand(player);
-    dealerScore = calculateHand(dealer);
+    showHands();
 }
 
 function showHand(hand, score) {
@@ -58,12 +79,15 @@ function showHand(hand, score) {
     return cards + " score: " + score;
 }
 
-clearTable();
-dealInitialCards();
-outputArea.innerHTML = showHand(player, playerScore) + "<br>" + showHand(dealer, dealerScore);
-
-function clearTable() {
-    outputArea.innerHTML = '';
+function showHands(stayed = false) {
+    playerScore = calculateHand(player);
+    dealerScore = calculateHand(dealer);
+    outputArea.innerHTML = showHand(player, playerScore) + "<br>" + showHand(dealer, dealerScore);
+    let winner = determineWinner(stayed);
+    winnerArea.innerHTML = winner;
+    if (winner !== "") {
+        hideGameButton();
+    }
 }
 
 // Calculate Hand Value
@@ -74,14 +98,84 @@ function calculateHand(cards) {
     });
     cards.forEach(function (card) {
         if (card.value == 'A') { score += 1; }
-        else if (card.value == 'J' || card.value == "Q" || card.value == "K") { score += 10; }
-        else score += 1*card.value;
+        else if (card.value == "J" || card.value == "Q" || card.value == "K") { score += 10; }
+        else score += 1 * card.value;
     });
     if (found && score <= 21 - 10) { score += 10; }
     return score;
 };
 
+// start a new game
+function startNewGame() {
+    showGameButton();
+    deck = [];
+    player = [];
+    dealer = [];
+    shuffleDeck();
+    dealInitialCards();
+};
 
+//  determinate the winner
+function hasBlackJack(hand, score) {
+    return hand.length == 2 && score == 21;
+}
+
+function isBust(score) {
+    return score > 21;
+}
+
+function determineWinner(stayed) {
+    if (isBust(playerScore)) {
+        return dealerWins + "; player bust";
+    }
+    else if (isBust(dealerScore)) {
+        return playerWins;
+    }
+    else if (dealerCards.length == 5 && dealerScore <= 21) {
+        return dealerWins + "; 5 cards under 21";
+    }
+    else if (stayed) {
+        if (dealerScore == playerScore) {
+            return draw;
+        }
+        else if (playerScore > dealerScore) {
+            return playerWins;
+        }
+        else if (dealerScore > playerScore) {
+            return dealerWins + "; larger score";
+        }
+    }
+    else {
+        let dealerBJ = hasBlackJack(dealer, dealerScore);
+        let playerBJ = hasBlackJack(player, playerScore);
+        if (playerBJ && dealerBJ) {
+            return draw;
+        }
+        else if (dealerBJ == true) {
+            return dealerWins + "; black jack";
+        }
+    }
+
+    return "";
+}
+
+
+function showGameButton() {
+    newGame.style.visibility = 'hidden';
+    hit.style.visibility = 'visible';
+    stay.style.visibility = 'visible';
+}
+// 
+function hideGameButton() {
+    newGame.style.visibility = 'visible';
+    hit.style.visibility = 'hidden';
+    stay.style.visibility = 'hidden';
+}
+
+// create constant variable
+const dealerWins = "Dealer Win!";
+const playerWins = "You Win!";
+const draw = "Draw";
 
 // create table to pick unicode cards
 
